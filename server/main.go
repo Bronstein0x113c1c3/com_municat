31211637ae1b7d3a19ac04f4780bf223971362b4
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2024-06-25 15:27:40 +0700

    for real_machine, not dockergit add . git add .

--- server/main.go
+++ server/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"server/step"
+	"strings"
 	// "github.com/google/uuid"
 )
 
 func main() {
+	fmt.Println("port: ")
+	var port int
+	fmt.Scanln(&port)
+	fmt.Println("use http3, if yes, press something contain y or Y, no otherwise: ")
+	var http3 bool
+	var x string
+	fmt.Scanln(&x)
+	if strings.Contains(x, "Y") || strings.Contains(x, "y") {
+		http3 = true
+	} else {
+		http3 = false
+	}
 	defer log.Println("turn off successfully!!!")
 	log.Println("initiating the server....")
-	serv := step.ServInit("", 8080)
+	serv := step.ServInit("", port)
 	defer func() {
 		close(serv.Input)
 		close(serv.Incoming)
 		close(serv.Leaving)
 	}()
 
-	helper, lis, err := step.ConnInit(serv, false)
+	helper, lis, err := step.ConnInit(serv, http3)
 	if err != nil {
 		log.Fatalf("server initation got error: %v \n", err)
 

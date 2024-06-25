2bce16a913a4d8edc4546f814315719584355b8e
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2024-06-25 13:20:31 +0700

    client rewriting

--- client/main.go
+++ client/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"client/control"
-	"client/input"
 	"client/networking"
-	"client/output"
-	pb "client/protobuf"
-	"client/transmitting"
-	"context"
+	"client/step"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 	"sync"
-
-	"google.golang.org/grpc/metadata"
 )
 
 func main() {
@@ -29,58 +20,46 @@ func main() {
 	var passcode string
 	fmt.Scanln(&passcode)
 
+
+
+
 	log.Println("initiating the signal....")
-	signal_chan := make(chan struct{})
-	command_chan := make(chan string)
-	ctx_signal, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
-	defer stop()
-	md_data := metadata.Pairs("passcode", passcode)
-	ctx_client := metadata.NewOutgoingContext(ctx_signal, md_data)
-	ctx1, cancel1 := context.WithCancel(ctx_signal)
+	signal_chan, command_chan, ctx_signal, ctx_client, ctx1, ctx2, stop, cancel1, cancel2 := step.Signal(passcode)
 	defer cancel1()
-	ctx2, cancel2 := context.WithCancel(ctx_signal)
 	defer cancel2()
 	log.Println("signal initiation done, connecting to the server....")
+
+
+
 
 	client, err := networking.InitV3("192.168.1.2:8080", "caller", ctx_client)
 	if err != nil {
 		log.Fatalf("error before connecting: %v \n", err)
 	}
-	client.Send(&pb.ClientREQ{
-		Request: &pb.ClientREQ_Signal{
-			Signal: &pb.ClientSignal{},
-		},
-	})
-	_, err = client.Recv()
-	if err != nil {
+	if err := step.TestConn(client); err != nil {
 		log.Fatalf("error after connection: %v \n", err)
 	}
 	defer client.CloseSend()
-	wg := &sync.WaitGroup{}
+
+
+
 
 	log.Println("connected, init the i/o...")
-	input, err := input.InputInit(DefaultChannels, DefaultSampleRate, DefaultFrameSize, DefaultOpusDataLength, wg)
+	wg := &sync.WaitGroup{}
+	input, output, data_chan, err := step.IOinit(DefaultChannels, DefaultSampleRate, DefaultFrameSize, DefaultOpusDataLength, wg)
 	if err != nil {
-		return
+		log.Fatalf("Error encountered when initializing I/O: %v \n", err)
+		// return
 	}
-	data_chan := make(chan []byte, 1000)
-	// defer close(data_chan)
-	output, err := output.OutputInit(DefaultChannels, DefaultSampleRate, DefaultFrameSize, data_chan)
-	if err != nil {
-		return
-	}
-
-	wg.Add(1)
+	defer close(data_chan)
 	log.Println("io done, start processing....")
+
+
+
+	
 	fmt.Println("please press ctrl+c anytime you want to stop, please :)")
-
-	go input.Process()
-	go transmitting.Send(input.GetChannel(), client, name)
-	go transmitting.Receive(data_chan, ctx1, client)
-	go output.Process()
-	go control.Command(signal_chan, command_chan)
-	go control.Control(ctx2, command_chan, signal_chan, input, output, stop)
-
+	wg.Add(1)
+	step.Serve(input, output, data_chan, signal_chan, command_chan, client, ctx1, ctx2, name, stop)
 	<-ctx_signal.Done()
 	defer wg.Wait()
 }
